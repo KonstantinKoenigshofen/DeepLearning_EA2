c3f2bb4d169ae41009c479682febeb74efbdb385
Konstantin Königshofen <koenigshofn11ko@gmail.com>
2026-05-20 14:26:58 +0200

    leakyRelu

--- script.js
+++ script.js
@@ -237,13 +237,13 @@ function createModel() {
     model.add(tf.layers.dense({
         inputShape: [1], 
         units: 100, 
-        activation: 'tanh'
+        activation: 'leakyRelu'
     }));
 
     // 2. Hidden Layer
     model.add(tf.layers.dense({
         units: 100,
-        activation: 'tanh'
+        activation: 'leakyRelu'
     }));
 
      // 3. Hidden Layer
