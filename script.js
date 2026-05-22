d5e681fea5a76352f027d41131b86ae328a97c9d
Konstantin Königshofen <koenigshofn11ko@gmail.com>
2026-05-22 13:04:41 +0200

    50/50

--- script.js
+++ script.js
@@ -159,7 +159,7 @@ function loadData() {
 //
 // Daten aufteilen
 //
-function splitData(data, splitRatio = 0.7) {
+function splitData(data, splitRatio = 0.5) {
     // 1. Kopie erstellen, damit das Original-Array unverändert bleibt
     const shuffled = [...data];
 
