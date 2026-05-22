7176ab0dd33f7434ce8d379ae5d6f56c8f6e8fcb
Konstantin Königshofen <koenigshofn11ko@gmail.com>
2026-05-22 13:07:57 +0200

    Epochen

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ async function main() {
     // Modell erstellen
     ///*
     console.log("--- A2: Clean Model ---");
-    const cleanModel = await getOrTrainModel('cleanModel', trainData, testData, 120);
+    const cleanModel = await getOrTrainModel('cleanModel', trainData, testData, 80);
 
     //
     // A3: Zweites Modell mit verrauschten Daten trainieren (Best-Fit)
@@ -49,7 +49,7 @@ async function main() {
 
     //Modell erstellen
     console.log("--- A3: Best-Fit Model ---");
-    const bestModel = await getOrTrainModel('bestFitModel', noisyTrainData, noisyTestData, 120);
+    const bestModel = await getOrTrainModel('bestFitModel', noisyTrainData, noisyTestData, 80);
         
     //
     // A4: Zweites Modell mit verrauschten Daten trainieren (Over-Fit)
@@ -57,7 +57,7 @@ async function main() {
 
     //Modell erstellen
     console.log("--- A4: Over-Fit Model ---");
-    const overfitModel = await getOrTrainModel('overfitModel', noisyTrainData, noisyTestData, 500);
+    const overfitModel = await getOrTrainModel('overfitModel', noisyTrainData, noisyTestData, 400);
     //*/
 
     // HIER NEUE IMPLEMENTIERUNG!!!!!!!!!!!!!!!!!!!!!!
@@ -112,7 +112,7 @@ async function main() {
 
     console.log("Visualisierung abgeschlossen!");
 
-    //await downloadModels(cleanModel, bestModel, overfitModel);
+    await downloadModels(cleanModel, bestModel, overfitModel);
 
 
 
